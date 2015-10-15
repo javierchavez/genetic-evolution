8a120b3bcc9a34f4ed22bc4bee5bff74333b88e1
Javier C <youracow@gmail.com>
2015-10-15 14:59:47 -0600

    added files to new vcreature pkg

---
+++ src/main/java/vcreature/Game.java
@@ -0,0 +1,34 @@
+package vcreature;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.renderer.RenderManager;
+import com.jme3.system.AppSettings;
+
+/**
+ * @author Javier
+ */
+public class Game extends SimpleApplication {
+
+    public static void main(String[] args) {
+        final AppSettings appSettings = new AppSettings(true);
+        appSettings.setTitle("Biacode - BANG!");
+        final Game game = new Game();
+        game.setSettings(appSettings);
+        game.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+   
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        super.simpleUpdate(tpf);
+    }
+
+    @Override
+    public void simpleRender(RenderManager rm) {
+        super.simpleRender(rm);
+    }
+}
