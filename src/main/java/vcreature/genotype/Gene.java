9433b0f43f734731b65a7f9fc6ea91a52f5ae135
Javier C <youracow@gmail.com>
2015-10-18 06:34:38 -0600

    removed unused code

--- src/main/java/vcreature/genotype/Gene.java
+++ src/main/java/vcreature/genotype/Gene.java
@@ -1,18 +1,13 @@
 package vcreature.genotype;
 
 
-import java.util.HashMap;
-import java.util.Map;
-
 public abstract class Gene<T extends Gene<?>>
 {
-  private HashMap<Integer, Double> linkedGeneWeights;
   private GeneType geneType;
   private boolean active;
 
   public Gene(GeneType geneType)
   {
-    linkedGeneWeights = new HashMap<>();
     this.geneType = geneType;
     this.active = true;
   }
@@ -34,25 +29,6 @@ public abstract class Gene<T extends Gene<?>>
     return geneType;
   }
 
-  /**
-   * Edge weights
-   *
-   * @return
-   */
-  public HashMap<Integer, Double> getLinkedGeneWeights()
-  {
-    return linkedGeneWeights;
-  }
-
-  /**
-   * Set the weights
-   *
-   * @param linkedGeneWeights
-   */
-  public void setLinkedGeneWeights(HashMap<Integer, Double> linkedGeneWeights)
-  {
-    this.linkedGeneWeights = linkedGeneWeights;
-  }
 
 
   public boolean isActive()
@@ -66,21 +42,10 @@ public abstract class Gene<T extends Gene<?>>
   }
 
 
-  public HashMap<Integer, Double> cloneLinkedGeneWeights()
-  {
-    HashMap<Integer, Double> newGeneLinkedWeights = new HashMap<>();
-    for (Map.Entry<Integer, Double> entry : getLinkedGeneWeights().entrySet())
-    {
-      int index = entry.getKey();
-      double weight = entry.getValue();
-      newGeneLinkedWeights.put(index, weight);
-    } return newGeneLinkedWeights;
-  }
-
   @Override
   public String toString()
   {
-    return "|" + getGeneType().toString() + "|" + linkedGeneWeights.toString();
+    return "|" + getGeneType().toString() + "|";
   }
 }
 
