27b24e4e8a2b1a40c6573a5c3a498a2e60099df8
Alex Baker <alexebaker@alexebaker.com>
2015-10-18 17:33:05 -0600

    generate some blocks (no collision detection, rotation, or neurons yet)

--- src/main/java/vcreature/RandomCreature.java
+++ src/main/java/vcreature/RandomCreature.java
@@ -37,7 +37,8 @@ public class RandomCreature extends Creature
     body.clear();
     Block root = generateBlock();
     body.add(root);
-    addBlocks(root);
+    //addBlocks(root);
+    generateBlock(generateBlock(generateBlock(root)));
     placeOnGround();
   }
 
@@ -72,7 +73,7 @@ public class RandomCreature extends Creature
     Vector3f size = genRandSize(rand);
 
     float x = 0f;
-    float y = size.y;
+    float y = 0f;
     float z = 0f;
     Vector3f center = new Vector3f(x, y, z);
 
@@ -83,30 +84,124 @@ public class RandomCreature extends Creature
   {
     Random rand = new Random();
     Vector3f size = genRandSize(rand);
-    Vector3f parentCenter = new Vector3f();
-    parentCenter = parent.getStartCenter(parentCenter);
     Vector3f parentSize = new Vector3f(parent.getSizeX()/2, parent.getSizeY()/2, parent.getSize()/2);
+    float[] angles = {0,0,0};
 
     int side = rand.nextInt(6);
+    Vector3f pivot;
+    Vector3f parentPivot;
+    Vector3f axis;
+    Vector3f parentAxis;
+
     float x = 0;
     float y = 0;
     float z = 0;
 
     switch (side) {
       case 0: // XY plane 1
+        x = (parent.getSizeX()/2) - (rand.nextFloat()*parent.getSizeX());
+        y = (parent.getSizeY()/2) - (rand.nextFloat()*parent.getSizeY());
+        z = parentSize.z;
+        parentPivot = new Vector3f(x, y, z);
+        pivot = new Vector3f(0, 0, -size.z);
+        if (rand.nextBoolean())
+        {
+          parentAxis = Vector3f.UNIT_X;
+          axis = Vector3f.UNIT_X;
+        }
+        else
+        {
+          parentAxis = Vector3f.UNIT_Y;
+          axis = Vector3f.UNIT_Y;
+        }
         break;
       case 1: // XY plane 2
+        x = (parent.getSizeX()/2) - (rand.nextFloat()*parent.getSizeX());
+        y = (parent.getSizeY()/2) - (rand.nextFloat()*parent.getSizeY());
+        z = -parentSize.z;
+        parentPivot = new Vector3f(x, y, z);
+        pivot = new Vector3f(0, 0, size.z);
+        if (rand.nextBoolean())
+        {
+          parentAxis = Vector3f.UNIT_X;
+          axis = Vector3f.UNIT_X;
+        }
+        else
+        {
+          parentAxis = Vector3f.UNIT_Y;
+          axis = Vector3f.UNIT_Y;
+        }
         break;
       case 2: // XZ plane 1
+        x = (parent.getSizeX()/2) - (rand.nextFloat()*parent.getSizeX());
+        y = parentSize.y;
+        z = (parent.getSize()/2) - (rand.nextFloat()*parent.getSize());
+        parentPivot = new Vector3f(x, y, z);
+        pivot = new Vector3f(0, -size.y, 0);
+        if (rand.nextBoolean())
+        {
+          parentAxis = Vector3f.UNIT_X;
+          axis = Vector3f.UNIT_X;
+        }
+        else
+        {
+          parentAxis = Vector3f.UNIT_Z;
+          axis = Vector3f.UNIT_Z;
+        }
         break;
       case 3: // XZ plane 2
+        x = (parent.getSizeX()/2) - (rand.nextFloat()*parent.getSizeX());
+        y = -parentSize.y;
+        z = (parent.getSize()/2) - (rand.nextFloat()*parent.getSize());
+        parentPivot = new Vector3f(x, y, z);
+        pivot = new Vector3f(0, size.y, 0);
+        if (rand.nextBoolean())
+        {
+          parentAxis = Vector3f.UNIT_X;
+          axis = Vector3f.UNIT_X;
+        }
+        else
+        {
+          parentAxis = Vector3f.UNIT_Z;
+          axis = Vector3f.UNIT_Z;
+        }
         break;
       case 4: // YZ plane 1
+        x = parentSize.x;
+        y = (parent.getSizeY()/2) - (rand.nextFloat()*parent.getSizeY());
+        z = (parent.getSize()/2) - (rand.nextFloat()*parent.getSize());
+        parentPivot = new Vector3f(x, y, z);
+        pivot = new Vector3f(-size.x, 0, 0);
+        if (rand.nextBoolean())
+        {
+          parentAxis = Vector3f.UNIT_Y;
+          axis = Vector3f.UNIT_Y;
+        }
+        else
+        {
+          parentAxis = Vector3f.UNIT_Z;
+          axis = Vector3f.UNIT_Z;
+        }
         break;
       default: // YZ plane 2
+        x = -parentSize.x;
+        y = (parent.getSizeY()/2) - (rand.nextFloat()*parent.getSizeY());
+        z = (parent.getSize()/2) - (rand.nextFloat()*parent.getSize());
+        parentPivot = new Vector3f(x, y, z);
+        pivot = new Vector3f(size.x, 0, 0);
+        if (rand.nextBoolean())
+        {
+          parentAxis = Vector3f.UNIT_Y;
+          axis = Vector3f.UNIT_Y;
+        }
+        else
+        {
+          parentAxis = Vector3f.UNIT_Z;
+          axis = Vector3f.UNIT_Z;
+        }
         break;
     }
-    return null;
+    return addBlock(angles, size, parent, parentPivot, pivot, parentAxis, axis);
   }
 
   private float genRandDim(Random rand)
