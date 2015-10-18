8fee50d5ea09537dde210004507929ea3776f252
Javier C <youracow@gmail.com>
2015-10-18 06:51:28 -0600

    single point crossover

--- src/main/java/vcreature/genotype/GeneticEngine.java
+++ src/main/java/vcreature/genotype/GeneticEngine.java
@@ -1,6 +1,8 @@
 package vcreature.genotype;
 
 
+import java.util.Random;
+
 public class GeneticEngine
 {
   public static Genome performCrossOver(Genome mother, Genome father)
@@ -29,6 +31,49 @@ public class GeneticEngine
     }
 
     return son;
+  }
+
+
+  public static Genome[] singlePoint(Genome mom, Genome dad) {
+
+    Random rand = new Random();
+
+    int size = mom.size();
+
+    // Create the Genomes for the twin children.
+    Genome g1 = new Genome();
+    Genome g2 = new Genome();
+
+
+    int interval = size / 5;
+    int transition = rand.nextInt(size - (2 * interval)) + interval;
+
+
+    for (int i = 0; i < size; i++) {
+      Gene parentGeneA = mom.getGenes().get(i).clone();
+      Gene parentGeneB = dad.getGenes().get(i).clone();
+      Gene childGeneA;
+      Gene childGeneB;
+
+
+      try {
+        if (i < transition) {
+          childGeneA = parentGeneA.clone();
+          childGeneB = parentGeneB.clone();
+        } else {
+          childGeneA = parentGeneB.clone();
+          childGeneB = parentGeneA.clone();
+        }
+
+        g1.append(childGeneA);
+        g2.append(childGeneB);
+      } catch (IllegalArgumentException ex) {
+        return null;
+      }
+    }
+
+    Genome[] children = {g1, g2};
+    return children;
   }
 
 
