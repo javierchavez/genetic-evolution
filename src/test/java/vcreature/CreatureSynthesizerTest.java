310fcd448953242bb267fe06b08fb1dc635b8b7e
Javier C <youracow@gmail.com>
2015-10-19 21:44:30 -0600

    test for creature syn

---
+++ src/test/java/vcreature/CreatureSynthesizerTest.java
@@ -0,0 +1,29 @@
+package vcreature;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CreatureSynthesizerTest
+{
+  MainSim app;
+  CreatureSynthesizer synthesizer;
+  @Before
+  public void setUp() throws Exception
+  {
+    synthesizer = new CreatureSynthesizer();
+  }
+
+  @Test
+  public void testEncode() throws Exception
+  {
+
+
+  }
+
+  @Test
+  public void testDecode() throws Exception
+  {
+
+  }
+}
