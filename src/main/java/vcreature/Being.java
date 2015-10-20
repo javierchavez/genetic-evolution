8175ee6ce922ec9ce562e15e9dc9d34b9489be0c
Javier C <youracow@gmail.com>
2015-10-19 21:47:23 -0600

    getter for body blocks

--- src/main/java/vcreature/Being.java
+++ src/main/java/vcreature/Being.java
@@ -2,8 +2,14 @@ package vcreature;
 
 
 import vcreature.genotype.Genome;
+import vcreature.phenotype.Block;
 import vcreature.phenotype.Creature;
 
+/**
+ * Wrapper for Genome and Creature
+ *
+ * The population will consist of many Beings
+ */
 public class Being /*Comparable<Being>*/
 {
 
@@ -20,4 +26,15 @@ public class Being /*Comparable<Being>*/
   private float fitness;
 
 
+  public Block[] getBodyBlocks ()
+  {
+    int totalBlocks = phenotype.getNumberOfBodyBlocks();
+    Block[] blocks = new Block[totalBlocks];
+    for (int i = 0; i < totalBlocks; i++)
+    {
+      blocks[i] = phenotype.getBlockByID(i);
+    }
+
+    return blocks;
+  }
 }
