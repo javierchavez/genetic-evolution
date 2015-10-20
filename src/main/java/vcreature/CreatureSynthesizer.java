edbd5c0527125936fee7006fb944d06328eb29d8
Javier C <youracow@gmail.com>
2015-10-20 14:01:41 -0600

    added comments

--- src/main/java/vcreature/CreatureSynthesizer.java
+++ src/main/java/vcreature/CreatureSynthesizer.java
@@ -24,16 +24,26 @@ public class CreatureSynthesizer extends Synthesizer<Creature, Genome>
   {
     Chromosome chromosome = new Chromosome();
 
+    // set the height
     chromosome.h = new HeightAllele().setValue(blockByID.getSizeY());
+    // set the width
     chromosome.w = new WidthAllele().setValue(blockByID.getSizeX());
+    // set the length
     chromosome.l = new LengthAllele().setValue(blockByID.getSize());
+    // set the index
+    chromosome.i = new IndexAllele().setValue(blockByID.getID());
 
+    // create a site (for the joint)
     Site s = new Site();
+
     s.childIndex = blockByID.getID();
     s.parentIndex = blockByID.getIdOfParent();
+    // set the site
     chromosome.j = new JointSiteAllele().setValue(s);
+    // set the angle (orientation)
     chromosome.o = new JointAngleAllele().setValue(blockByID.getJointAngle());
 
+    // need to turn all the neurons into inputs and operators
     for (int i = 0; i < blockByID.getNeuronTable().size(); i++)
     {
       for (int j = 0; j < 5; j++)
@@ -42,7 +52,6 @@ public class CreatureSynthesizer extends Synthesizer<Creature, Genome>
 
       }
     }
-
     return chromosome;
   }
 
