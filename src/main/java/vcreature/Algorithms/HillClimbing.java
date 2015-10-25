479620c53d74410c7fea9e245f5c19e1518b7d89
Dominic Salas <dominic.salas@gmail.com>
2015-10-25 11:28:00 -0600

    Didn't mean to sync my game file...it's strictly for my testing

--- src/main/java/vcreature/Algorithms/HillClimbing.java
+++ src/main/java/vcreature/Algorithms/HillClimbing.java
@@ -1,8 +1,15 @@
 package vcreature.Algorithms;
 
+import vcreature.Population;
+
 /**
  * Created by dsalas on 10/25/15.
  */
 public class HillClimbing
 {
+
+  public HillClimbing (Population population)
+  {
+    
+  }
 }
