154c21a52df2dff9d7d20f12804073771ff621b4
cari <cari_martinez123@yahoo.com>
2015-11-04 18:34:52 -0700

    implementing neural net climb strategy

--- src/main/java/vcreature/Algorithms/NeurlNetClimbStrategey.java
+++ src/main/java/vcreature/Algorithms/NeurlNetClimbStrategey.java
@@ -3,6 +3,8 @@ package vcreature.Algorithms;
 import vcreature.genotype.NeuralNode;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 
 public class NeurlNetClimbStrategey<V> implements HillClimbStrategy<ArrayList<NeuralNode>, V>
@@ -12,10 +14,50 @@ public class NeurlNetClimbStrategey<V> implements HillClimbStrategy<ArrayList<Ne
   @Override
   public V climb(ArrayList<NeuralNode> part)
   {
-    if (part.size() > 0)
+
+    if(part.size() < 1)
     {
-      strategy.climb(part.get(0));
+      Random rnd = new Random();
+      int index1 = rnd.nextInt(part.size());
+      int index2 = rnd.nextInt(part.size());
+      int index3 = rnd.nextInt(part.size());
+      int x = rnd.nextInt(5);
+
+      switch (x)
+      {
+        case 1:
+        {
+          for (NeuralNode node : part)
+          {
+            strategy.climb(node);
+          }
+
+        }
+        case 2:
+        {
+          strategy.climb(part.get(index1));
+        }
+        case 3:
+        {
+          strategy.climb(part.get(index1));
+          strategy.climb(part.get(index2));
+        }
+        case 4:
+        {
+          strategy.climb(part.get(index1));
+          strategy.climb(part.get(index2));
+          strategy.climb(part.get(index3));
+        }
+        case 5:
+
+        {
+          Collections.shuffle(part);
+
+        }
+      }
+
     }
+
     return null;
   }
 }
