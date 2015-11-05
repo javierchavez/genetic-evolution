61645fce79a398cd4ec7f20b28edd9ddcaa0f0fd
Alex Baker <alexebaker@alexebaker.com>
2015-11-04 19:53:58 -0700

    root node dimension mutation strategy

--- src/main/java/vcreature/Algorithms/RootGeneDimensionClimbStrategy.java
+++ src/main/java/vcreature/Algorithms/RootGeneDimensionClimbStrategy.java
@@ -2,7 +2,11 @@ package vcreature.Algorithms;
 
 
 import com.jme3.math.Vector3f;
+import vcreature.genotype.Gene;
 import vcreature.genotype.Genome;
+import vcreature.phenotype.Block;
+
+import java.util.Random;
 
 public class RootGeneDimensionClimbStrategy<V> implements HillClimbStrategy<Genome, V>
 {
@@ -10,9 +14,35 @@ public class RootGeneDimensionClimbStrategy<V> implements HillClimbStrategy<Geno
   @Override
   public V climb(Genome part)
   {
+    Random rand = new Random();
+    float scaleFactor = rand.nextBoolean() ? 1.05f : 0.95f;
     // only mutate diminution
-    part.get(0).getDimensions(vector3f);
+    part.getRoot().getDimensions(vector3f);
 
+    vector3f.x *= scaleFactor;
+    vector3f.y *= scaleFactor;
+    vector3f.z *= scaleFactor;
+
+    if (vector3f.x < 0.5f || vector3f.y < 0.5f || vector3f.z < 0.5f)
+    {
+      return null;
+    }
+
+    if (Block.max(vector3f) > 10*Block.min(vector3f))
+    {
+      return null;
+    }
+
+    part.getRoot().setDimensions(vector3f);
+
+    for (Gene neighbor : part.neighbors(part.getRoot()))
+    {
+      neighbor.getEffector().getParent(vector3f);
+      vector3f.x *= scaleFactor;
+      vector3f.y *= scaleFactor;
+      vector3f.z *= scaleFactor;
+      neighbor.getEffector().setParent(vector3f);
+    }
     return null;
   }
 }
