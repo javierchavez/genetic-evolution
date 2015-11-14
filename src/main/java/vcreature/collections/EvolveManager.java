055145d7bddaaae0c14dca81346f15d8acb1106f
Javier C <youracow@gmail.com>
2015-11-14 13:10:42 -0700

    fixed bug for HC

--- src/main/java/vcreature/collections/EvolveManager.java
+++ src/main/java/vcreature/collections/EvolveManager.java
@@ -86,6 +86,12 @@ public class EvolveManager extends Thread implements Savable
               isEvolving = false;
               System.out.println("[END]: CROSS");
             }
+            else
+            {
+              mutate = true;
+              isEvolving = false;
+              interrupt();
+            }
           }
         }
 
